0c0c2838f5c858686373f8440c3de607754c911c
mboldacova <barcaforall@gmail.com>
2024-01-26 23:00:08 -0800

    New files

---
+++ lesson_2_task_0.py
@@ -0,0 +1,5 @@
+my_age_as_string = input("What is your age? ")
+print("Your age: " + my_age_as_string)
+my_age = int(my_age_as_string)
+my_age = my_age + 1
+print("Your age in a year will be", my_age)
---
+++ lesson_2_task_2.py
@@ -0,0 +1,13 @@
+year_as_string = input("Enter a year: ")
+year = int(year_as_string)
+
+def is_year_leap():
+    if year % 4 == 0:
+        print("Year", year, ":", True)
+
+    else:
+        print("Year", year, ":", False)
+
+is_year_leap()
+
+        
